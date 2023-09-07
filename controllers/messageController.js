7a522678096270f8d76ca4e41ce1ff7f817c82c0
Tony W <bobacrypto2@gmail.com>
2023-09-06 23:36:13 -0700

    Create message create get controller method | Add message form template

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,2 +1,11 @@
+const Message = require("../models/message");
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
+
+exports.message_create_get = asyncHandler(async (req, res, next) => {
+  res.render("message_form", {
+    title: "Create Message",
+  });
+});
+
+exports.message_create_post = asyncHandler(async (req, res, next) => {});
--- routes/account.js
+++ routes/account.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require("passport");
 
 const user_controller = require("../controllers/userController");
+const message_controller = require("../controllers/messageController");
 
 // Create account
 router.get("/create", user_controller.user_create_get);
@@ -19,5 +20,8 @@ router.post(
 // Secret | Join Club
 router.get("/secret/:id", user_controller.user_secret_get);
 router.post("/secret/:id", user_controller.user_secret_post);
+
+router.get("/new-message/create/:id", message_controller.message_create_get);
+router.post("/new-message/create/:id", message_controller.message_create_post);
 
 module.exports = router;
